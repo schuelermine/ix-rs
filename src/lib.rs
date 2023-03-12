10c15ddaa6e7a4f8ffddd2ac927bcadf980baab2
Anselm Schüler <mail@anselmschueler.com>
2023-03-12 14:44:33 +0100

    shuffle things around

--- src/lib.rs
+++ src/lib.rs
@@ -126,7 +126,51 @@ pub trait Ix: PartialOrd + Sized {
     fn range_size_checked(min: Self, max: Self) -> Option<usize>;
 }
 
-mod macros;
-use macros::impl_ix_numeric;
+macro_rules! assert_ordered {
+    ($min: expr, $max: expr) => {
+        if $min > $max {
+            panic!("min is greater than max");
+        }
+    };
+}
+
+macro_rules! assert_in_range {
+    ($min: expr, $max: expr, $ix: expr) => {
+        if $ix < $min {
+            panic!("ix is outside range (self < min)");
+        } else if $ix > $max {
+            panic!("ix is outside range (self > max)");
+        }
+    };
+}
+
+macro_rules! impl_ix_numeric {
+    ($($t: ty),+) => {
+        $(
+            impl $crate::Ix for $t {
+                type Range = ::core::ops::RangeInclusive<$t>;
+                fn range(min: Self, max: Self) -> Self::Range {
+                    assert_ordered!(min, max);
+                    min..=max
+                }
+                fn index_checked(self, min: Self, max: Self) -> Option<usize> {
+                    assert_ordered!(min, max);
+                    assert_in_range!(min, max, self);
+                    usize::try_from(self - min).ok()
+                }
+                fn in_range(self, min: Self, max: Self) -> bool {
+                    assert_ordered!(min, max);
+                    min <= self && self <= max
+                }
+                fn range_size_checked(min: Self, max: Self) -> Option<usize> {
+                    assert_ordered!(min, max);
+                    usize::try_from(max - min)
+                        .ok()
+                        .and_then(|n| n.checked_add(1))
+                }
+            }
+        )+
+    };
+}
 
 impl_ix_numeric!(u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, usize, isize);
